b16d8f371b0a97c7618ce2c705cb84198ec99336
Joel Mosemann <mosemann@hotmail.com>
2018-01-05 23:17:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ function increaseRankBy(n) {
 }
 }
 
+function deepestChild() {
+  var grandNode = document.querySelector('#grand-node');
+  var deepestNode = grandNode.children[0];
+  for (var i = 0; !deepestNode.children[i]; i) {
+    deepestNode = deepestNode.children[0]
+  }
+  return deepestNode;
+}
